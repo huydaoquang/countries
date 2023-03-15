fd7d29dab6f5bacd9cc2928e185868da7f59310d
huy dao <you@example.com>
2023-03-15 15:41:19 +0700

    upadate search and filter css next

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import { useContext } from "react";
+import styled from "styled-components";
+
 import "./App.css";
 import Header from "./Components/Header";
 import MainContent from "./Components/MainContent";
+import { ThemeContext } from "./Components/ThemeContext/themeContext";
 
 function App() {
+  const themeContext = useContext(ThemeContext);
   return (
-    <div className="App">
+    <AppContainer className={themeContext.theme}>
       <Header />
-      <MainContent />
-    </div>
+      <ContentContainer>
+        <MainContent />
+      </ContentContainer>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+`;
+
+const ContentContainer = styled.div`
+  max-width: 1280px;
+  display: block;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 12px;
+`;
--- src/Components/MainContent/SearchAndFilter/Search.jsx
+++ src/Components/MainContent/SearchAndFilter/Search.jsx
@@ -1,17 +1,69 @@
 import { MdSearch } from "react-icons/md";
+import styled from "styled-components";
 
 const Search = () => {
   return (
-    <div>
+    <SearchPane>
       <h3>Search Country</h3>
-      <div>
-        <input type="text" />
-        <div>
-          <MdSearch />
+      <SearchElement>
+        <input type="text" placeholder="Input the enter to search..." />
+        <div style={{ width: "40px", height: "100%" }}>
+          <MdSearch className="icon" />
         </div>
-      </div>
-    </div>
+      </SearchElement>
+    </SearchPane>
   );
 };
 
 export default Search;
+
+const SearchPane = styled.div`
+  max-width: 320px;
+  width: 100%;
+  margin-top: 20px;
+
+  h3 {
+    font-size: 18px;
+    font-weight: 600;
+    text-shadow: var(--TextShadow);
+  }
+`;
+
+const SearchElement = styled.div`
+  margin-top: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  height: 34px;
+  background-color: #fff;
+  box-shadow: var(--BoxShadow);
+  border-radius: 4px;
+  overflow: hidden;
+
+  .icon {
+    height: 100%;
+    width: 100%;
+    padding: 2px;
+    text-align: center;
+    background-color: #aaa !important;
+    box-shadow: none !important;
+    opacity: 75%;
+    transition: opacity 0.2s linear;
+
+    &:hover {
+      opacity: 1;
+      cursor: pointer;
+    }
+  }
+
+  input {
+    border: 0;
+    outline: none;
+    width: 100%;
+    font-size: 18px;
+    font-weight: 500;
+    margin: 0 8px;
+  }
+`;
