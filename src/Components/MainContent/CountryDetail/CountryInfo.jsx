f75d5a9d0d5f4a612fca79fb8d8f75df62dfcfe0
huy dao <you@example.com>
2023-03-16 10:52:02 +0700

    country detail next

--- src/Components/MainContent/CountryDetail/CountryInfo.jsx
+++ src/Components/MainContent/CountryDetail/CountryInfo.jsx
@@ -1,7 +1,58 @@
+import styles from "./CountryInfo.module.scss";
+
 const CountryInfo = (props) => {
   return (
-    <div>
-      <h3>this is info country</h3>
+    <div className={styles.countryInfo}>
+      <h3 className={styles.countryName}>Bhutan</h3>
+      <table>
+        <tbody>
+          <tr>
+            <td className={styles.countryInfo__title}>Native Name</td>
+            <td>:</td>
+            <td className={styles.countryInfo__value}>brug-yul</td>
+          </tr>
+          <tr>
+            <td className={styles.countryInfo__title}>Official</td>
+            <td>:</td>
+            <td className={styles.countryInfo__value}>brug-yul</td>
+          </tr>
+          <tr>
+            <td className={styles.countryInfo__title}>Population</td>
+            <td>:</td>
+            <td className={styles.countryInfo__value}>brug-yul</td>
+          </tr>
+          <tr>
+            <td className={styles.countryInfo__title}>Region</td>
+            <td>:</td>
+            <td className={styles.countryInfo__value}>brug-yul</td>
+          </tr>
+          <tr>
+            <td className={styles.countryInfo__title}>Sub Region</td>
+            <td>:</td>
+            <td className={styles.countryInfo__value}>brug-yul</td>
+          </tr>
+          <tr>
+            <td className={styles.countryInfo__title}>Capital</td>
+            <td>:</td>
+            <td className={styles.countryInfo__value}>brug-yul</td>
+          </tr>
+          <tr>
+            <td className={styles.countryInfo__title}>Currencies</td>
+            <td>:</td>
+            <td className={styles.countryInfo__value}>brug-yul</td>
+          </tr>
+          <tr>
+            <td className={styles.countryInfo__title}>Languages</td>
+            <td>:</td>
+            <td className={styles.countryInfo__value}>brug-yul</td>
+          </tr>
+          <tr>
+            <td className={styles.countryInfo__title}>Border Countries</td>
+            <td>:</td>
+            <td className={styles.countryInfo__value}>brug-yul</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
